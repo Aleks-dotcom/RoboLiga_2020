0c8c94fa914152bce9268e74a7ed2fa72c4c2184
Aleks-dotcom <aleksandermundjar3@gmail.com>
2020-07-22 11:26:32 +0200

    klesce testing (new design, Jure out <3) 22.7.2020 .

--- nabiralec.py
+++ nabiralec.py
@@ -444,6 +444,7 @@ def get_next_diseaset(rp, hives, team_my_tag, HIVE_IGNORE_LIST):
 
 def drop_cage(motor_medium):
     global cage_lifted
+    print("Dropping cage: "+str(cage_lifted))
     if (cage_lifted):
         return
     cage_lifted = False
@@ -453,6 +454,7 @@ def drop_cage(motor_medium):
 
 def lift_cage(motor_medium):
     global cage_lifted
+    print("Lifting cage: "+str(cage_lifted))
     if (not cage_lifted):
         return
     cage_lifted = True
