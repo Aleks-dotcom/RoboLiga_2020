b758ead38776100b7191156d9c4443e00c62b851
Aleks-dotcom <aleksandermundjar3@gmail.com>
2020-08-06 16:15:21 +0200

    POC za astar, probamo najprej s pythonom

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git add nabiralec.py
-git commit -m "hardening podvozje, obogatitev testing 29.7.2020 ."
+git commit -m "prejsnji commit je untested potestiramo nasledni tedn. 6.8.2020 ."
 git push
